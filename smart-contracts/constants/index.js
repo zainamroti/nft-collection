d908b8013db33b32bb65ba9313a3b112a1a84a2f
zainamroti <sayedzainjeelani@gmail.com>
2022-04-24 07:31:22 +0500

    add hardhat, contract configs ++

---
+++ smart-contracts/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xb0744Ff8B35d8713AddEC5d59Ad74334126704F8";
+// URL to extract Metadata for a ZeeCards NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ smart-contracts/contracts/ZeeCards.sol
@@ -0,0 +1,84 @@
+//SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.4;
+import "../node_modules";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol"; 
+
+contract ZeeCards is ERC721Enumerable, Ownable {
+     /**
+       * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+       * token will be the concatenation of the `baseURI` and the `tokenId`.
+       */
+      string _baseTokenURI;
+
+      //_price is the price of one ZeeCards NFT 
+      uint256 public _price = 0.01 ether; 
+
+      //_paused is used to pause the contract in case of an emergency  
+      bool public _paused;  
+
+      // max no: of zeeCards
+      uint256 public maxTokenIds = 20;
+
+      //total number of tokenIds minted;
+      uint256 public tokenIds;
+
+      // Whitelist contract instance
+      IWhitelist whitelist; 
+
+      //boolean to keep track of whether presale started or not 
+      bool public presaleStarted;
+
+      //timestamp for when presale would end 
+      uint256 public presaleEnd;
+
+      modifier onlyWhenNotPaused {
+          require(!_paused, "Contract currently paused");
+          _;
+      }
+    
+     /**
+       * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+       * name in our case is `Crypto Devs` and symbol is `CD`.
+       * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+       * It also initializes an instance of whitelist interface.
+       */
+      constructor (string memory baseURI, address whitelistContract) ERC721("Crypto Devs", "CD") {
+          _baseTokenURI = baseURI;
+          whitelist = IWhitelist(whitelistContract);
+      }
+
+    /**
+      * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
+      * returned an empty string for the baseURI
+      */
+      function _baseURI() internal view virtual override returns (string memory) {
+          return _baseTokenURI;
+      }
+
+      /**
+      * @dev setPaused makes the contract paused or unpaused
+       */
+      function setPaused(bool val) public onlyOwner {
+          _paused = val;
+      }
+
+      /**
+      * @dev withdraw sends all the ether in the contract to the
+      owner of the contract
+      */
+      function withdraw() public onlyOwner {
+          address _owner = owner();
+          uint256 amount = address(this).balance; 
+          (bool sent,) = _owner.call{value: amount}("");
+          require(sent, "Failed to send Ether");
+      }
+
+      //Function to receive Ether. msg.data must be empty
+      receive() external payable {}
+
+      //Fallback function is called when msg.data is not empty
+      fallback() external payable {}
+
+}
